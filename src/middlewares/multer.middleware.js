75b2a494a673c0e5ef942b47525d47c44a892a07
choubeyshubhanker <shubhankerc@gmail.com>
2024-03-21 16:16:18 +0530

    cloudinary and multer setup for file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    //   cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, file.originalname)  // never use original file name as it can overwrite files with same names
+    }
+  })
+  
+  export const upload = multer({ storage: storage })
