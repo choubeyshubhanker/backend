e9de60a4e5d6070884a69150a2cf692a2b043239
choubeyshubhanker <shubhankerc@gmail.com>
2024-03-21 13:24:31 +0530

    api error and response util setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.error("ERROR: ", error);
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.info(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.error("MONGO DB connection failed!! ",err);
+});
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) =>{
+    (res, req, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err));
+    }
+};
+
+
+
+/*
+// Using TRY CATCH.......
+const asyncHandler = (fn) => async (req,res,next) => {
+    try{
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+*/
+
+export { asyncHandler }
